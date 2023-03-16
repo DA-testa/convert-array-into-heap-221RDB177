b240e69b48871a08b7947dbd95d7c6a71d1aef14
221RDB177 <125874315+221RDB177@users.noreply.github.com>
2023-03-16 15:41:44 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,57 +1,59 @@
-# python3
-
-
 def build_heap(data):
     swaps = []
+    n = len(data)
     
-     n = len(data)
-
-    # This loop sifts down all non-leaf nodes to create a max-heap
-    for i in range(n // 2 - 1, -1, -1):
-        j = i
-        while 2 * j + 1 < n:
-            k = j
-            # Check if left child is smaller than current node
-            if data[2 * j + 1] < data[k]:
-                k = 2 * j + 1
-            # Check if right child is smaller than current node and left child
-            if 2 * j + 2 < n and data[2 * j + 2] < data[k]:
-                k = 2 * j + 2
-            if j != k:
-                # Swap current node with smaller child
-                data[j], data[k] = data[k], data[j]
-                swaps.append((j, k))
-                j = k
-            else:
-                break
+    # Starting from the last parent node, recursively sort the subtree rooted at i
+    for i in range(n // 2, -1, -1):
+        sort(data, i, swaps)
 
     return swaps
 
 
-def main():
+def sort(data, i, swaps):
+    n = len(data)
+    right = 2 * i + 2
+    left = 2 * i + 1
+    lir = i 
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    # If the left child is smaller than the current root, set it as the new root
+    if left < n and data[left] < data[lir]:
+        lir = left
+        
+    # If the right child is smaller than the current root, set it as the new root
+    if right < n and data[right] < data[lir]:
+        lir = right
+        
+    # If the root has changed, swap the values and add the indices to the swaps list
+    if i != lir:
+        data[i], data[lir] = data[lir], data[i]
+        swaps.append((i, lir))
+        
+        # Recursively sort the subtree rooted at the new root
+        sort(data, lir, swaps)
 
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+def main():
+    test = input()
+    
+    # If F is in the input, read data from a file
+    if 'F' in test:
+        test_file = input()
+        with open("tests/" + test_file, 'r') as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+    else:
+        # Otherwise, read data from the command line
+        n = int(input())
+        data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
+    
+    # Build the heap and get the swaps list
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
+    
+    # Print the indices of each swap
     for i, j in swaps:
         print(i, j)
 
